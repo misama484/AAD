3828d590fd0670270199a98c35d15d97404c2b50
Zephyrus\Miguel <misan484@hotmail.com>
2022-12-21 13:47:01 +0100

    Se ingresan los datos ok en bd, recupera datos ok

--- src/t4/ejercicios/Principal.java
+++ src/t4/ejercicios/Principal.java
@@ -33,6 +33,7 @@ public class Principal {
 		BufferedReader br = new BufferedReader(fr);
 		String linea = br.readLine();
 		ArrayList<String> libros = new ArrayList<String>();
+		ArrayList<Libro> biblioteca = new ArrayList<Libro>();
 		
 		while(linea != null) {		//cortar el string linea en subString para crear el objeto libro
 			System.out.println(linea);
@@ -51,24 +52,43 @@ public class Principal {
 				}
 			}
 			Libro book = new Libro(Integer.parseInt(arrayLibro[0]), arrayLibro[1], arrayLibro[2], Integer.parseInt(arrayLibro[3]), Integer.parseInt(arrayLibro[4]), arrayLibro[5],Integer.parseInt(arrayLibro[6]));
+			biblioteca.add(book);
 			
-			System.out.println("Libro => " + book.toString());
 		}
 		
+		for (Libro lib : biblioteca) {
+			System.out.println("Libro => " + lib.toString());
+		}
 		
-		
+		System.out.println("\n===== ACCESO A MONGO =====\n");
 		//INICIAMOS LA CONEXION
 		//CREAMOS LA BASE DE DATOS
 		MongoClient mongoClient = new MongoClient("localhost", 27017);
-		List<String> DBName = mongoClient.getDatabaseNames();
+		//List<String> DBName = mongoClient.getDatabaseNames();
 		//JOptionPane.showMessageDialog(null, DBName.toString());
 		MongoDatabase database = mongoClient.getDatabase("Biblioteca2");	//CAMBIAR A NOMBRE DE NUEVA BASE DE DATOS PARA PODER CREARLA
 		MongoCollection<Document> collection = database.getCollection("Libros2");
 		
 		//OPERACIONES CRUD	
+		/*
+		//creamos una lista de documentos y le anyadimo cada documento con los datos de los libros del ArrayList biblioteca
+		//insertamos la coleccion en la bd creada.
+		ArrayList<Document> listaDocs = new ArrayList<Document>();
+		for(Libro libro : biblioteca) {
+			Document doc = new Document();
+			doc.append("id", libro.getId());
+			doc.append("titulo", libro.getTitulo());
+			doc.append("autor", libro.getAutor());
+			doc.append("anyoNacimiento", libro.getAnyNacimiento());
+			doc.append("anyoPublicacion", libro.getAnyoPublicacion());
+			doc.append("editorial", libro.getEditorial());
+			doc.append("numPaginas", libro.getNumPaginas());
+			listaDocs.add(doc);			
+		}
+		collection.insertMany(listaDocs);
+		*/
 		
-		
-		//recuperar lista de elementos en coleccion(Tabla)
+		//RECUPERAR LISTA DE ELEMENTOS DE LA COLECION(Tabla)
 		//MongoCursor<Document> cursor = collection.find(Bson query = eq("Titulo", "Lazarillo de Tormes")).iterator(); //CON EL FILTRO NO FUNCIONA, NO RECONOCE Bson
 		MongoCursor<Document> cursor = collection.find().iterator();
 		while(cursor.hasNext()) {
@@ -76,7 +96,7 @@ public class Principal {
 			//JOptionPane.showMessageDialog(null, cursor.next().toJson());
 			
 			JSONObject obj = new JSONObject(cursor.next().toJson());	
-			System.out.println(obj.getString("Titulo"));
+			System.out.println(obj.getString("titulo"));
 			//System.out.println(cursor.next().toJson());
 		}
 	
