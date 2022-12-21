a449794da0e9ea28d662bb5a649eb903c50f3d76
Zephyrus\Miguel <misan484@hotmail.com>
2022-12-21 12:53:10 +0100

    Preparando la creacion de bd en mongo desde codigo, catalogo de libros ya en ArrayList

--- src/t4/ejercicios/Principal.java
+++ src/t4/ejercicios/Principal.java
@@ -33,36 +33,40 @@ public class Principal {
 		BufferedReader br = new BufferedReader(fr);
 		String linea = br.readLine();
 		ArrayList<String> libros = new ArrayList<String>();
-		ArrayList<String> libro = new ArrayList<String>();
+		
 		while(linea != null) {		//cortar el string linea en subString para crear el objeto libro
 			System.out.println(linea);
-			libros.add(linea);
-			//String[] arrayDatos = linea.split(";");			
+			libros.add(linea);		
 			linea = br.readLine();
 		}
 		
 		//POSIBILIDAD DE PASAR DIRECTAMENTE LA LINEA CON LOS DATOS DEL LIBRO COMO JSON DIRECTAMENTE A MONGO??
+		libros.remove(0);//eliminamos primera linea que contiene el nombre de los campos
 		
-		for(String lib : libros) {
+		for(String lib : libros) {	//Creamos objetos libro y Anyadimos los libros a la lista de libros (Pasamos los datos numericos a int, con una comrpbacion para si falta algun dato, que lo sustituya por "0".)
 			String[] arrayLibro = lib.split(";");
-			for(int i = 0; i < arrayLibro.length ; i++) {	//extraer datos del array(datos de cada libro) y crear objeto Libro
-				String titulo = arrayLibro[i];
+			for(int i = 0; i < arrayLibro.length; i++) {
+				if(arrayLibro[i] == "") {
+					arrayLibro[i] = "0";
+				}
 			}
+			Libro book = new Libro(Integer.parseInt(arrayLibro[0]), arrayLibro[1], arrayLibro[2], Integer.parseInt(arrayLibro[3]), Integer.parseInt(arrayLibro[4]), arrayLibro[5],Integer.parseInt(arrayLibro[6]));
 			
-			System.out.println("Libro => " + lib);
+			System.out.println("Libro => " + book.toString());
 		}
 		
 		
 		
-		/*
-		//Iniciamos la conexion
+		//INICIAMOS LA CONEXION
+		//CREAMOS LA BASE DE DATOS
 		MongoClient mongoClient = new MongoClient("localhost", 27017);
 		List<String> DBName = mongoClient.getDatabaseNames();
 		//JOptionPane.showMessageDialog(null, DBName.toString());
-		MongoDatabase database = mongoClient.getDatabase("Biblioteca");	//CAMBIAR A NOMBRE DE NUEVA BASE DE DATOS PARA PODER CREARLA
-		MongoCollection<Document> collection = database.getCollection("Libros");
+		MongoDatabase database = mongoClient.getDatabase("Biblioteca2");	//CAMBIAR A NOMBRE DE NUEVA BASE DE DATOS PARA PODER CREARLA
+		MongoCollection<Document> collection = database.getCollection("Libros2");
 		
 		//OPERACIONES CRUD	
+		
 		
 		//recuperar lista de elementos en coleccion(Tabla)
 		//MongoCursor<Document> cursor = collection.find(Bson query = eq("Titulo", "Lazarillo de Tormes")).iterator(); //CON EL FILTRO NO FUNCIONA, NO RECONOCE Bson
@@ -79,7 +83,7 @@ public class Principal {
 		
 		//con esto cerrmos la conexion a la bd
 		mongoClient.close();
-*/
+
 	}
 
 }
